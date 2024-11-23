61b1a93d0999f392315e47c6f74fbb4d48a2c3d5
Nguyenduchien1206 <hiennd.nguyenduc2001@gmail.com>
2024-11-24 01:22:50 +0700

    update

--- Assets/Scripts/Photon/DemoPhotonNetwork.cs
+++ Assets/Scripts/Photon/DemoPhotonNetwork.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
 
-public class DemoPhotonNetwork : MonoBehaviour
+public class DemoPhotonNetwork : MonoBehaviourPunCallbacks
 {
+    [SerializeField] Button CreateRoomBtn;
+    [SerializeField] Button JoinRoomBtn;
+    [SerializeField] TMP_InputField RoomName;
+    [SerializeField] TextMeshProUGUI StatusText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    public override 
+
+
 }
